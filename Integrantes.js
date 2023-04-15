b96ccb16bb1153da17f9c9ab9bb30ab5a58b476d
Luis139Pastrana <126751620+Luis139Pastrana@users.noreply.github.com>
2023-04-15 07:07:01 -0400

    Add files via upload

---
+++ Integrantes.js
@@ -0,0 +1,18 @@
+function info(x){
+    console.log(x);
+}
+/*document.getElementById("Nombre").innerHTML = "Ing Molina";
+
+switch (integrante){
+    case 0:
+        console.log("Ing. Molina");
+        break;
+    case 1:
+        console.log("Ing. Molina");
+        break;
+    default:
+        console.log("no se presiono en ningun integrante")
+        break;
+}*/
+
+console.log("Hol mundo");
